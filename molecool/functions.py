f48ce2ed0bcc490cf83468f54680413f4f064e9b
Tyler <tws21@pitt.edu>
2019-11-07 14:00:26 -0500

    Removed a line required for jupyter notebook

--- molecool/functions.py
+++ molecool/functions.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
 
-%matplotlib notebook
+#%matplotlib notebook
 
 def calculate_distance(rA, rB):
     # This function calculates the distance between two points given as numpy arrays.
